c2c3e00580dc2b39ea031c803c173392197e5d31
Guilherme Rangel <gui.vg16@hotmail.com>
2020-03-18 22:26:09 -0300

    implemented table view cell

--- ObjectiveC_Challenge/Controller/CustomCell.h
+++ ObjectiveC_Challenge/Controller/CustomCell.h
@@ -1,18 +1,22 @@
 //
-//  NSObject+CustomCell.h
+//  CustomCell.h
 //  ObjectiveC_Challenge
 //
 //  Created by Guilherme Rangel on 18/03/20.
 //  Copyright © 2020 Vitor Demenighi. All rights reserved.
 //
 
-
-#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CustomCell : UITableViewCell
+@interface CustomCell : NSObject
+
+@property(strong, nonatomic)NSString * strImg;
+@property(strong, nonatomic)NSString * strLbl1;
+@property(strong, nonatomic)NSString * strLbl2;
+
+
 
 @end
 
--- ObjectiveC_Challenge/Controller/CustomCell.m
+++ ObjectiveC_Challenge/Controller/CustomCell.m
@@ -1,16 +1,13 @@
 //
-//  NSObject+CustomCell.m
+//  CustomCell.m
 //  ObjectiveC_Challenge
 //
 //  Created by Guilherme Rangel on 18/03/20.
 //  Copyright © 2020 Vitor Demenighi. All rights reserved.
 //
 
-#import "NSObject+CustomCell.h"
-
-#import <AppKit/AppKit.h>
-
-
-@implementation NSObject (CustomCell)
-
+#import "CustomCell.h"
+#import "ViewController.h"
+@implementation CustomCell
+@synthesize strImg, strLbl1, strLbl2;
 @end
